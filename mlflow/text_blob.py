9080e9f6b047d894fde22a3756f2b25f12f64406
Gao Heng <100184550+foxiegh@users.noreply.github.com>
2024-04-16 23:09:00 +0800

    MLFlow with Docker

---
+++ mlflow/text_blob.py
@@ -0,0 +1,96 @@
+import mysql.connector
+import pandas as pd
+import os
+from sqlalchemy import create_engine
+import re
+import gensim
+from gensim.corpora import Dictionary
+from gensim.models import LdaModel, CoherenceModel, LsiModel, HdpModel
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from gensim.models import CoherenceModel
+import yaml
+import nltk
+from textblob import TextBlob
+from sklearn.metrics import accuracy_score
+
+import mlflow
+from mlflow.models import infer_signature
+import mlflow.pyfunc
+
+#LOAD DATA
+with open('data/db_info.yml', 'r') as file:
+    data = yaml.safe_load(file)
+
+db_host = data["DATABASE_ENDPOINT"]
+db_user = data["DATABASE_USERNAME"]
+db_pw = data["DATABASE_PASSWORD"]
+db_name = data["DATABASE_NAME"]
+db_port = data["DATABASE_PORT"]
+
+dwh_host = data["DATAWH_ENDPOINT"]
+dwh_user = data["DATAWH_USERNAME"]
+dwh_pw = data["DATAWH_PASSWORD"]
+dwh_name = data["DATAWH_NAME"]
+dwh_port = data["DATAWH_PORT"]
+
+db_datawarehouse = mysql.connector.connect(
+	host=dwh_host,
+	user=dwh_user,
+	passwd=dwh_pw,
+	database=dwh_name,
+    auth_plugin=dwh_pw
+)
+engine = create_engine(f'mysql+pymysql://{dwh_user}:{dwh_pw}@{dwh_host}:{dwh_port}/{dwh_name}', echo=False)
+
+dwh = engine.connect()
+
+review_sql = '''
+SELECT * FROM review ORDER BY OverallID
+'''
+df = pd.read_sql(sql=review_sql, con=db_datawarehouse)
+dwh.close()
+
+reviews = df["CleanReviewText"].to_numpy()
+
+
+class TextB(mlflow.pyfunc.PythonModel):
+    def __init__(self):
+        pass
+
+    def predict(self, model_input, params=None):
+        res = []
+        for each in model_input:
+            res.append(TextBlob(each).sentiment.polarity)
+        return res
+
+model_path = "text_blob"
+model = TextB()
+y_pred = model.predict(reviews)
+y_pred = [0 if pred<=0 else 1 for pred in y_pred]
+y_truth = [0 if pred<=0 else 1 for pred in df["TextBlob_Review"]]
+accuracy = accuracy_score(y_truth, y_pred)
+
+# Define the signature associated with the model
+signature = infer_signature(reviews, model.predict(reviews))
+
+
+mlflow.set_tracking_uri(uri="http://localhost:5000")
+mlflow.set_experiment("MLFlow for Project - TextBlob")
+
+with mlflow.start_run():
+    mlflow.set_tag("Model", "Text Blob")
+
+    mlflow.log_metric("accuracy", accuracy)
+
+    mlflow.pyfunc.save_model(
+        path=model_path,
+        python_model=model,
+        input_example=reviews,
+        signature=signature,
+        pip_requirements=None,
+    )
+
+
+loaded_model = mlflow.pyfunc.load_model(model_path)
+
