5815e6fc0921b11f1dfff702b22311c71cd54816
e0727131 <e0727131@u.nus.edu>
2024-04-23 22:57:49 +0800

    Fixed bug where it throws an error when the end date of the date picker is not supplied.

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -16,6 +16,7 @@ from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from nltk import pos_tag
 from streamlit import components
+from datetime import datetime
 import pyLDAvis
 import pyLDAvis.gensim_models
 
@@ -69,15 +70,21 @@ if selected_label == "Customer Sentiment Analysis":
     st.title('🏂 MBS Customer Sentiment Analysis') 
 
     # this is filter by date_input
-    # min_date = complete_df['StayDate'].min()
-    # max_date = complete_df['StayDate'].max()
-    # date_range = st.date_input("Pick the period you want to analyse", (min_date, max_date),  max_value = max_date, min_value = min_date, format="DD/MM/YYYY")
+    min_date = df['StayDate'].min()
+    max_date = df['StayDate'].max()
+    date_range = st.date_input("Pick the period you want to analyse", (min_date, max_date),  max_value = max_date, min_value = min_date, format="DD/MM/YYYY", key="sentiment")
     
+    # # Create a date range slider
+    # min_date = min_date.to_pydatetime()
+    # max_date = max_date.to_pydatetime()
+    # st.write(min_date)
+    # selected_dates = st.slider("Select Date Range", min_value=min_date, max_value=max_date, value=(min_date, max_date))
+
     # this is filter by year only
-    year_list = list(df.StayDateYear.unique())[::-1]
-    selected_year = st.selectbox('Select a year', year_list, index=len(year_list)-1)
-    df_selected_year = df[df.StayDateYear == selected_year]
-    df_selected_year_sorted = df_selected_year.sort_values(by="Rating", ascending=False)
+    # year_list = list(df.StayDateYear.unique())[::-1]
+    # selected_year = st.selectbox('Select a year', year_list, index=len(year_list)-1)
+    # df_selected_year = df[df.StayDateYear == selected_year]
+    # df_selected_year_sorted = df_selected_year.sort_values(by="Rating", ascending=False)
 
 
     #co11, col2 = st.columns([2,3])
@@ -85,30 +92,38 @@ if selected_label == "Customer Sentiment Analysis":
     # either filter by year or filter by range of dates
 
     #filter by year
-    filter_year = (df["StayDateYear"] == selected_year)
-    filtered_df_by_year = df[filter_year]
+    # filter_year = (df["StayDateYear"] == selected_year)
+    # filtered_df_by_year = df[filter_year]
 
     # filter by range of dates
-    # earliest_date = pd.to_datetime(date_range[0])
-    # latest_date = pd.to_datetime(date_range[1])
+    earliest_date = pd.to_datetime(date_range[0])
+    latest_date = max_date if len(date_range) == 1 else pd.to_datetime(date_range[1]) # ensure that there will always be a valid end date
 
-    # filtered_df_by_year = complete_df[(complete_df['StayDate'] >= earliest_date) & (complete_df['StayDate'] <= latest_date)]
+    # filtered_df_by_year = df[(df['StayDate'] >= earliest_date) & (df['StayDate'] <= latest_date)]
+    filtered_df_by_year = df[(df['StayDate'] >= earliest_date) & (df['StayDate'] <= latest_date)]
     # st.write(filtered_df_by_year)
 
     #with co11:
     #st.write("""
             ### Bar Chart of Customer's Reviews""")
     # fig, ax = plt.subplots()
+
     rating_counts = filtered_df_by_year['Rating'].value_counts().sort_index()
+    
     # ax.bar(counts.index, counts.values)
     # ax.set_xlabel('Ratings')
     # ax.set_ylabel('Count')
     # ax.set_xticks(range(1, 6))
     # ax.set_xticklabels(range(1, 6))
     # st.pyplot(fig)
-    fig1 = px.bar(x = rating_counts.index, y = rating_counts.values, 
-                  title= "Count of Customer Reviews", labels={"x": "Ratings", "y": "Count"})
-    st.plotly_chart(fig1)
+
+    if not rating_counts.empty:
+        fig1 = px.bar(x = rating_counts.index, y = rating_counts.values, 
+                    title= "Count of Customer Reviews", labels={"x": "Ratings", "y": "Count"})
+        st.plotly_chart(fig1)
+    else:
+        st.markdown("<h2 style='text-align: center;'>No data available for the selected period.</h2>", unsafe_allow_html=True)
+        # st.write("No data available.")
 
     #with col2: 
     # st.write("""
@@ -126,27 +141,54 @@ if selected_label == "Customer Sentiment Analysis":
     # ax.legend(wedges, counts.index, title='Sentiment', loc='center left', bbox_to_anchor=(1, 0.5), )
     # plt.setp(autotexts, size=8, weight="bold")  # Adjust the size and weight of percentage labels
     # st.pyplot(fig)
-    fig2 = px.pie(sentiment_counts, values = "count", names = sentiment_counts["Text_Sentiment"],
-                  title= "Proportion of Sentiment for Customer Reviews", 
-                  color=sentiment_counts["Text_Sentiment"],
-                  color_discrete_sequence=["sky blue", "crimson"])
-    st.plotly_chart(fig2)
+
+    if not sentiment_counts.empty:
+        fig2 = px.pie(sentiment_counts, values = "count", names = sentiment_counts["Text_Sentiment"],
+                    title= "Proportion of Sentiment for Customer Reviews", 
+                    color=sentiment_counts["Text_Sentiment"],
+                    color_discrete_sequence=["sky blue", "crimson"])
+        st.plotly_chart(fig2)
+    # else:
+    #     st.markdown("<h2 style='text-align: center;'>No data available for the selected period.</h2>", unsafe_allow_html=True)
 
 else: #selected_label = "Topic Modelling"
     st.title('🏂 Topic Modelling')
+
+    # this is filter by date_input
+    min_date = df['StayDate'].min()
+    max_date = df['StayDate'].max()
+    date_range = st.date_input("Pick the period you want to analyse", (min_date, max_date),  max_value = max_date, min_value = min_date, format="DD/MM/YYYY", key="topics")
     
+    # filter by range of dates
+    earliest_date = pd.to_datetime(date_range[0])
+    latest_date = max_date if len(date_range) == 1 else pd.to_datetime(date_range[1]) # ensure that there will always be a valid end date
+    filtered_df_by_year = df[(df['StayDate'] >= earliest_date) & (df['StayDate'] <= latest_date)]
+
     #Obtain all the nouns from the text
-    df = preprocess_data(df, "CleanReviewText")
+    df = preprocess_data(filtered_df_by_year, "CleanReviewText")
 
     #Positive Reviews Topic Modelling
     positive_df = df[df["Text_Sentiment"] == 1]
 
-    positive_html_string = topic_modelling(positive_df)
-    components.v1.html(positive_html_string, width=1200, height=800, scrolling=True)
+    st.markdown("<h1 style='text-align: center;'>Topics for Positive Reviews</h1>", unsafe_allow_html=True)
+    if not positive_df.empty:
+        positive_html_string = topic_modelling(positive_df)
+        components.v1.html(positive_html_string, width=1200, height=800, scrolling=True)
+    else:
+        st.markdown("<h2 style='text-align: center;'>No data available for the selected period.</h2>", unsafe_allow_html=True)
+
+    # Line break
+    st.markdown("---")
 
     #Negative Reviews Topic Modelling
     negative_df = df[df["Text_Sentiment"] == 0]
 
-    negative_html_string = topic_modelling(negative_df)
-    components.v1.html(negative_html_string, width=1200, height=800, scrolling=True)
+    st.markdown("<h1 style='text-align: center;'>Topics for Negative Reviews</h1>", unsafe_allow_html=True)
+    if not negative_df.empty:
+        negative_html_string = topic_modelling(negative_df)
+        components.v1.html(negative_html_string, width=1200, height=800, scrolling=True)
+    else:
+        st.markdown("<h2 style='text-align: center;'>No data available for the selected period.</h2>", unsafe_allow_html=True)
 
+        # else:
+        # st.markdown("<h2 style='text-align: center;'>No data available for the selected period.</h2>", unsafe_allow_html=True)
--- streamlit_app/db_utils.py
+++ streamlit_app/db_utils.py
@@ -29,20 +29,20 @@ def get_data():
         engine = create_engine(f'mysql+pymysql://{dwh_user}:{dwh_pw}@{dwh_host}:{dwh_port}/{dwh_name}', echo=False)
         review_sql = '''
         SELECT * 
-        FROM review'''
-        review_df = pd.read_sql(review_sql, engine)
+        FROM review;'''
+        review_df = pd.read_sql(review_sql, engine.raw_connection())
 
         time_sql = '''
         SELECT * 
-        FROM time'''
+        FROM time;'''
 
-        time_df = pd.read_sql(time_sql, engine)
+        time_df = pd.read_sql(time_sql, engine.raw_connection())
 
         fact_sql = '''
         SELECT * 
-        FROM fact'''
+        FROM fact;'''
 
-        fact_df = pd.read_sql(fact_sql, engine)
+        fact_df = pd.read_sql(fact_sql, engine.raw_connection())
 
         join_sql = '''
         SELECT  fact.Text_Sentiment, fact.Title_Sentiment, time.OverallID, time.StayDate, time.StayDateYear, time.StayDateMonth, time.StayDateDayOfWeek, time.StayDateDay, time.StayDateWeek,
@@ -50,12 +50,13 @@ def get_data():
         FROM fact
         INNER JOIN review ON fact.OverallID = review.OverallID
         INNER JOIN time ON time.OverallID = fact.OverallID
-        ORDER BY time.OverallID
+        ORDER BY time.OverallID;
         '''
 
 
-        complete_df = pd.read_sql(join_sql, engine)
-    except:
+        complete_df = pd.read_sql(join_sql, engine.raw_connection())
+    except Exception as e:
+        st.write(e)
         st.error("Unable to connect to DB, please check error logs")
     return complete_df
 
